bba47189ffb806c04139cb36164732b029057cdd
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-04-26 18:24:06 +0200

    update bias

--- env.py
+++ env.py
@@ -11,10 +11,10 @@ class PosType(Enum):
     ROPE = "rope"
 
 model_configs = {
-    'small': dict(block_size=1024, vocab_size=50257, n_layer=12, n_head=12, n_embd=768,  dropout=0.0, bias=True, gqa_kv_head=4),
-    'medium': dict(block_size=1024, vocab_size=50257, n_layer=24, n_head=16, n_embd=1024, dropout=0.0, bias=True, gqa_kv_head=4),
-    'large': dict(block_size=1024, vocab_size=50257, n_layer=36, n_head=20, n_embd=1280, dropout=0.0, bias=True, gqa_kv_head=6),
-    'xl': dict(block_size=1024, vocab_size=50257, n_layer=48, n_head=25, n_embd=1600, dropout=0.0, bias=True, gqa_kv_head=6),
+    'small': dict(block_size=1024, vocab_size=50257, n_layer=12, n_head=12, n_embd=768,  dropout=0.0, bias=False, gqa_kv_head=4),
+    'medium': dict(block_size=1024, vocab_size=50257, n_layer=24, n_head=16, n_embd=1024, dropout=0.0, bias=False, gqa_kv_head=4),
+    'large': dict(block_size=1024, vocab_size=50257, n_layer=36, n_head=20, n_embd=1280, dropout=0.0, bias=False, gqa_kv_head=6),
+    'xl': dict(block_size=1024, vocab_size=50257, n_layer=48, n_head=25, n_embd=1600, dropout=0.0, bias=False, gqa_kv_head=6),
 }
 
 dataset_configs = {
