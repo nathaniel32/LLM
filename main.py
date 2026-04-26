9c87a441826a32259808c1d3d2d5a2c6d494e459
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-04-26 20:46:59 +0200

    update class

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Main:
         torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
 
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        dtype = 'float32'
+        dtype = 'float16'
         ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
         self.ctx = nullcontext() if self.device == 'cpu' else torch.amp.autocast(device_type=self.device, dtype=ptdtype)
         self.use_cache = use_cache
