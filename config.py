97bf2f2302a262a32817d6a140bbfd52d7eb4560
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-04-30 00:14:40 +0200

    update

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ class DatasetConfig(BaseConfig):
     dir_path: Optional[str]
 
     def __post_init__(self):
-        root = os.path.dirname(os.path.abspath(__file__))
-        self.root_dir = os.path.join(root, 'datasets', self.name)
+        project_root = os.path.dirname(os.path.abspath(__file__))
+        self.root_dir = os.path.join(project_root, 'datasets', self.name)
 
     def prepare_dataset(self):
         import requests
