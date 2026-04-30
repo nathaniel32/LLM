062329f94ced4cbf484dda5d94bd5b1a61969b63
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-04-30 20:19:18 +0200

    update

--- check_params.py
+++ check_params.py
@@ -16,7 +16,7 @@ def check_config(attn_type):
         # self.qk_rope_head_dim = self.head_dim // 2
         # self.kv_lora_dim = self.head_dim * 2
         head_dim = m.n_embd // m.n_head
-        kv_lora_dim = head_dim * 2
+        kv_lora_dim = m.n_embd // 4 # head_dim * 2
         qk_rope_head_dim = head_dim // 2
         kv_size_per_layer = kv_lora_dim + qk_rope_head_dim
     else:
