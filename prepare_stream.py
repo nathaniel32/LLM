422b5681cfda1e5062df5392de524c665fc52760
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-04-30 02:25:55 +0200

    update

--- prepare_stream.py
+++ prepare_stream.py
@@ -4,12 +4,12 @@ import numpy as np
 import tiktoken
 from datasets import load_dataset
 
-def prepare_data(max_samples, val_ratio, root_dir="datasets", datasets="openwebtext"):
-    out_dir = os.path.join(root_dir, datasets)
+def prepare_data(max_samples, val_ratio, data_name, root_dir):
+    out_dir = os.path.join(root_dir, data_name)
     os.makedirs(out_dir, exist_ok=True)
 
     enc = tiktoken.get_encoding("gpt2")
-    dataset = load_dataset(datasets, split="train", streaming=True)
+    dataset = load_dataset(data_name, split="train", streaming=True)
 
     val_step = int(1 / val_ratio) if val_ratio > 0 else 0
     print({"val_step": val_step})
@@ -49,14 +49,19 @@ def read_bin_file(file_path, num_tokens=50):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--max_samples", type=int, default=1000000) #8013769
+    parser.add_argument("--max_samples", type=int) #8013769
     parser.add_argument("--val_ratio", type=float, default=0.01)
     parser.add_argument("--check_data", action="store_true")
+    parser.add_argument("--data_name", type=str, default='openwebtext')
+    parser.add_argument("--out_dir", type=str, default='datasets')
     args = parser.parse_args()
 
     if not args.check_data:
-        prepare_data(args.max_samples, args.val_ratio)
+        if args.max_samples:
+            prepare_data(max_samples=args.max_samples, val_ratio=args.val_ratio, data_name=args.data_name, root_dir=args.out_dir)
+        else:
+            print("max_samples not found!")
     else:
-        total_tokens, decoded_text = read_bin_file("datasets/openwebtext/val.bin", 500)
+        total_tokens, decoded_text = read_bin_file(os.path.join(args.out_dir, args.data_name, 'val.bin'), 500)
         print(f"- Total: {total_tokens}")
         print(f"- Text: \n{decoded_text}")
