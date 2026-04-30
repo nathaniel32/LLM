c5650508a485ded84d5277afda9dfd5437b51f85
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-04-30 18:18:22 +0200

    update

--- config.py
+++ config.py
@@ -152,25 +152,25 @@ class ModelType(Enum):
 
 class TrainType(Enum):
     RESEARCH = TrainConfig(
-        name="train_fast_research",
+        name="train_research",
         dtype='float16',
 
         batch_size=4,
         gradient_accumulation_steps=4,
 
-        max_iters=1000,
-        lr_decay_iters=1000,
-        warmup_iters=100,
-        eval_iters=25,
+        max_iters=2000,
+        lr_decay_iters=2000,
+        warmup_iters=200,
+        eval_iters=50,
 
         eval_interval=50,
         log_interval=10,
 
-        min_lr=1e-5,
-        learning_rate=3e-4,
+        min_lr=6e-5,
+        learning_rate=1e-3,
 
         grad_clip=1.0,
-        weight_decay=0.05,
+        weight_decay=0.01,
         beta1=0.9,
         beta2=0.95,
 
