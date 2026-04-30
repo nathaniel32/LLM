a1aa9be895fa896d96be2aede28f602b0e4ba2fe
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-04-30 17:07:46 +0200

    update config

--- config.py
+++ config.py
@@ -119,7 +119,7 @@ class DatasetType(Enum):
     SHAKESPEARE = DatasetConfig(name="shakespeare", url='https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt', dir_path=None)
 
 class ModelType(Enum):
-    RESEARCH = ModelConfig(name="model_research", block_size=512, vocab_size=50257, n_layer=2, n_head=25, n_embd=1600, dropout=0.0, bias=False)
+    RESEARCH = ModelConfig(name="model_research", block_size=512, vocab_size=50257, n_layer=2, n_head=24, n_embd=1536, dropout=0.0, bias=False)
     
     SMALL = ModelConfig(name="model_small", block_size=1024, vocab_size=50257, n_layer=12, n_head=12, n_embd=768, dropout=0.0, bias=False)
     MEDIUM = ModelConfig(name="model_medium", block_size=1024, vocab_size=50257, n_layer=24, n_head=16, n_embd=1024, dropout=0.0, bias=False)
@@ -158,12 +158,12 @@ class TrainType(Enum):
         batch_size=4,
         gradient_accumulation_steps=4,
 
-        max_iters=3000,
-        lr_decay_iters=3000,
-        warmup_iters=200,
-        eval_iters=50,
+        max_iters=1000,
+        lr_decay_iters=1000,
+        warmup_iters=100,
+        eval_iters=25,
 
-        eval_interval=100,
+        eval_interval=50,
         log_interval=10,
 
         min_lr=1e-5,
