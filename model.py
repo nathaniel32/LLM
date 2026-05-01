0281448ff4db436542297158ad4fceb1a64517f7
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-05-01 04:35:33 +0200

    update param

--- model.py
+++ model.py
@@ -236,14 +236,14 @@ class MultiHeadLatentAttention(BaseSelfAttention):
         super().__init__(configs)
         self.efficient = efficient
         
-        self.qk_nope_head_dim = self.head_dim // 4
+        self.q_lora_dim = 0
+        self.v_head_dim = self.head_dim
+        self.kv_lora_dim = configs.model_type.value.n_embd // 4
+
+        self.qk_nope_head_dim = self.head_dim
         self.qk_rope_head_dim = self.head_dim // 2
         self.qk_head_dim = self.qk_nope_head_dim + self.qk_rope_head_dim
-        self.v_head_dim = self.head_dim // 2
-
-        self.q_lora_dim = 0
-        self.kv_lora_dim = self.head_dim * 2 # configs.model_type.value.n_embd // 4
-
+        
         self.cache = KVCache(self.model_config.block_size) if not efficient else MLAKVCache(self.model_config.block_size, self.kv_lora_dim, self.qk_rope_head_dim)
 
         # Query compression
