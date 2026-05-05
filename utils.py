55b6e8a6744873b1ec12a1e979689021f926f157
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-05-05 23:34:16 +0200

    update

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import random
 import torch
 import numpy as np
 
-def set_seed(seed=1337):        
+def set_seed(seed=1234):        
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
