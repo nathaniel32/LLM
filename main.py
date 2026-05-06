c71621fd6692800f590791c7199f13a40c497e83
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-05-06 02:43:03 +0200

    update

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from model import Transformer
 import tiktoken
 from benchmark import Benchmark
 from config import Configs, AttnType, PosType, NormType, ModelType, args, args_configs
-from utils import set_seed
+from utils import set_seed, ModelContext
 
 class Main:
     def __init__(self, configs:Configs, use_cache=False, stream=False):
         set_seed()
 
-        self.configs = configs
+        self.model_context = ModelContext(configs=configs)
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[configs.train_type.value.dtype]
         self.ctx = nullcontext() if self.device == 'cpu' else torch.amp.autocast(device_type=self.device, dtype=ptdtype)
@@ -19,64 +19,8 @@ class Main:
         self.stream = stream
         self.benchmark = Benchmark(device=self.device)
 
-    def from_pretrained(self):
-        from transformers import GPT2LMHeadModel
-
-        # create a from-scratch initialized minGPT model
-        self.configs = Configs(flash=True, model_type=self.configs.model_type, attn_type=AttnType.MHA, pos_type=PosType.WPE, norm_type=NormType.LAYER)
-
-        model = Transformer(self.configs)
-        sd = model.state_dict()
-        sd_keys = sd.keys()
-        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
-
-        # init a huggingface/transformers model
-        model_hf = GPT2LMHeadModel.from_pretrained(self.configs.model_type.value.name)
-        sd_hf = model_hf.state_dict()
-
-        # copy while ensuring all of the parameters are aligned and match in names and shapes
-        sd_keys_hf = sd_hf.keys()
-        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
-        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
-        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
-        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
-        # this means that we have to transpose these weights when we import them
-        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
-        for k in sd_keys_hf:
-            if any(k.endswith(w) for w in transposed):
-                # special treatment for the Conv1D weights we need to transpose
-                assert sd_hf[k].shape[::-1] == sd[k].shape
-                with torch.no_grad():
-                    sd[k].copy_(sd_hf[k].t())
-            else:
-                # vanilla copy over the other parameters
-                assert sd_hf[k].shape == sd[k].shape
-                with torch.no_grad():
-                    sd[k].copy_(sd_hf[k])
-
-        return model
-    
-    def from_out(self, filename):
-        import os
-        
-        ckpt_path = os.path.join(self.configs.out_dir, filename)
-        
-        checkpoint = torch.load(ckpt_path, map_location=self.device)
-        self.configs = Configs(**checkpoint['args'])
-        
-        model = Transformer(self.configs)
-        state_dict = checkpoint['model']
-        unwanted_prefix = '_orig_mod.'
-        
-        for k,v in list(state_dict.items()):
-            if k.startswith(unwanted_prefix):
-                state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
-        
-        model.load_state_dict(state_dict)
-        return model
-    
     @torch.no_grad()
-    def generate(self, idx, model:Transformer, enc:tiktoken.Encoding, max_new_tokens, temperature=1.0, top_k=None, stop_token=False):
+    def generate(self, idx, enc:tiktoken.Encoding, max_new_tokens, temperature=1.0, top_k=None, stop_token=False):
         """
         Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
         the sequence max_new_tokens times, feeding the predictions back into the model each time.
@@ -85,7 +29,7 @@ class Main:
         eot_token = enc.encode('<|endoftext|>', allowed_special={'<|endoftext|>'})[0]
 
         if self.use_cache:
-            model.reset_cache()
+            self.model_context.model.reset_cache()
 
         self.benchmark.start()
 
@@ -95,7 +39,7 @@ class Main:
             else:
                 idx_cond = idx
 
-            logits, _ = model(idx_cond, use_cache=self.use_cache)
+            logits, _ = self.model_context.model(idx_cond, use_cache=self.use_cache)
             
             # pluck the logits at the final step and scale by desired temperature
             logits = logits[:, -1, :] / temperature
@@ -124,7 +68,7 @@ class Main:
                 text = enc.decode(idx[0].tolist())
                 print('\033[u\033[J' + text, end='', flush=True)
 
-            if self.configs.model_type.value.block_size < idx.size(-1):
+            if self.model_context.configs.model_type.value.block_size < idx.size(-1):
                 print("Block full!")
                 break
 
@@ -132,17 +76,17 @@ class Main:
 
         return idx
     
-    def warmup(self, model):
+    def warmup(self):
         print("Warming up GPU...")
 
         dummy_input = torch.randint(0, 50257, (1, 20), device=self.device)
 
-        model.eval()
+        self.model_context.model.eval()
 
         with torch.no_grad():
             with self.ctx:
                 for _ in range(5):
-                    _ = model(dummy_input, use_cache=self.use_cache)
+                    _ = self.model_context.model(dummy_input, use_cache=self.use_cache)
 
         if self.device == 'cuda':
             torch.cuda.synchronize()
@@ -150,15 +94,16 @@ class Main:
         print("Warm-up done.\n")
 
     def run(self, max_new_tokens, start, temperature=0.8, top_k=200, pretrained=False):
-        model = self.from_pretrained() if pretrained else self.from_out(filename='best_checkpoint.pt')
-        model.eval()
-        model.to(self.device)
+        self.model_context.from_pretrained() if pretrained else self.model_context.get_model(resume=True, device=self.device, filename='best_checkpoint.pt')
+        
+        self.model_context.model.eval()
+        self.model_context.model.to(self.device)
 
-        self.configs.in_training = False
+        self.model_context.configs.in_training = False
 
         label = "use_cache=True" if self.use_cache else "use_cache=False"
         print(f'\n[{label}]')
-        print(self.configs.info())
+        print(self.model_context.configs.info())
 
         #self.warmup(model)
 
@@ -168,7 +113,7 @@ class Main:
 
         with torch.no_grad():
             with self.ctx:
-                y = self.generate(x, model, enc, max_new_tokens, temperature=temperature, top_k=top_k)
+                y = self.generate(x, enc, max_new_tokens, temperature=temperature, top_k=top_k)
                 
         text = enc.decode(y[0].tolist())
 
--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 import random
 from typing import Optional
 from model import Transformer
-from config import Configs
+from config import Configs, AttnType, PosType, NormType
 
 def set_seed(seed=1234):        
     random.seed(seed)
@@ -34,6 +34,41 @@ class ModelContext:
     optimizer: Optional[torch.optim.AdamW] = None
     scaler: Optional[torch.amp.GradScaler] = None
     train_state: Optional[TrainState] = None
+
+    def from_pretrained(self):
+        from transformers import GPT2LMHeadModel
+
+        # create a from-scratch initialized minGPT model
+        self.configs = Configs(flash=True, model_type=self.configs.model_type, attn_type=AttnType.MHA, pos_type=PosType.WPE, norm_type=NormType.LAYER)
+
+        self.model = Transformer(self.configs)
+        sd = self.model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
+
+        # init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(self.configs.model_type.value.name)
+        sd_hf = model_hf.state_dict()
+
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
 
     def get_model(self, resume, device, filename='last_checkpoint.pt'):
         ckpt_path = os.path.join(self.configs.out_dir, filename)
