8f7c71d87d5804cfa2f88817f5484b321cfaf2c6
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-05-06 03:09:31 +0200

    update

--- train.py
+++ train.py
@@ -115,7 +115,7 @@ class Train:
         return out
     
     def train(self, resume=True):
-        self.model_context.get_model(resume=resume, device=self.device)
+        self.model_context.load_model(resume=resume, device=self.device)
         
         self.model_context.configs.in_training = True
         
